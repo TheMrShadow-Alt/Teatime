f9e756673675c8475a71624de5f6e4c1d4340185
MrShadow <camjekel@gmail.com>
2025-07-11 17:26:30 -0400

    Update track.js

--- api/track.js
+++ api/track.js
@@ -1,1 +1,8 @@
+let activeUsers = new Set();
 
+export default function handler(req, res) {
+  const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+  activeUsers.add(ip);
+  setTimeout(() => activeUsers.delete(ip), 60000); // 1 minute timeout
+  res.status(200).json({ activeUsers: activeUsers.size });
+}
